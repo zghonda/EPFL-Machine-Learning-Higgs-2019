92de5fb181049523d300b7b3fbb96f7373df2673
olivierlam97 <olivier.lam@epfl.ch>
2019-10-23 10:52:52 +0200

    prediction ok

--- scripts/proj1_helpers.py
+++ scripts/proj1_helpers.py
@@ -79,3 +79,50 @@ def build_k_indices(y, k_fold, seed):
     k_indices = [indices[k * interval: (k + 1) * interval]
                  for k in range(k_fold)]
     return np.array(k_indices)
+
+
+def group_indices(x):
+    """get the indices of the six groups.
+    Each column contains the range of indices of each group
+    There 3 main groups of particles divided according to their PRI_jet_num
+    and each main group is divided to two subgroups depending on if they have a DER_mass_MMC or not."""
+
+    # g0_ind_wo_mmc means indices of group 0 of the particles that don't have a DER_mass_MMC
+
+    g0_ind_wo_mmc = np.where((x[:, 22] == 0) & (np.isnan(x[:, 0])))
+    g0_ind_w_mmc = np.where((x[:, 22] == 0) & (~np.isnan(x[:, 0])))
+    g1_ind_wo_mmc = np.where((x[:, 22] == 1) & (np.isnan(x[:, 0])))
+    g1_ind_w_mmc = np.where((x[:, 22] == 1) & (~np.isnan(x[:, 0])))
+    g2_ind_wo_mmc = np.where((x[:, 22] >= 2) & (np.isnan(x[:, 0])))
+    g2_ind_w_mmc = np.where((x[:, 22] >= 2) & (~np.isnan(x[:, 0])))
+
+    return [g0_ind_wo_mmc, g0_ind_w_mmc, g1_ind_wo_mmc, g1_ind_w_mmc, g2_ind_wo_mmc, g2_ind_w_mmc]
+
+
+def replace_data(x, replace, by):
+    return np.where(x == replace, by, x)
+
+
+def drop_na_columns(x):
+    """Drops all columns that have only NaN values"""
+    result = x[:, ~np.all(np.isnan(x), axis=0)]
+    return result
+
+
+def preprocess_data(x_train, x_test):
+    """ Apply data pre processing and cleaning """
+    # data standardization
+    x_train, mean_tr, std_tr = standardize(x_train)
+    x_test = (x_test - mean_tr) / std_tr  # ref : https://bit.ly/2MytlBg
+
+    # drop columns that have only Nan values
+    x_train, x_test = drop_na_columns(x_train), drop_na_columns(x_test)
+
+    return x_train, x_test
+
+
+def performance_measure(y_pred, y):
+    """ Measures the accuracy of the predictions"""
+    diff = y_pred - y
+    n_correct = diff[diff == 0].shape[0]
+    return n_correct / len(y)
